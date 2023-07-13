15e0206ed01f9eadf5e119268bec35ba7a8ea917
Chis Williams <willchris917@gmail.com>
2023-07-13 14:30:14 +0530

    Update url

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { notFound } from "next/navigation";
 
 async function getData(id: string) {
-  const res = await fetch(`/api/posts/${id}`, {
+  const res = await fetch(process.env.NEXTAUTH_URL + `/api/posts/${id}`, {
     cache: 'no-store'
   })
   if (!res.ok) {
